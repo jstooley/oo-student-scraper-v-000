ad657cdcaf535343b4c366c098985991939c1822
Justin Tooley <jstooley823@gmail.com>
2017-09-20 05:24:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       binding.pry
-      Student.new(Scraper.scrape_index_page(student))
+      Student.new(student)
     end
 
   end
