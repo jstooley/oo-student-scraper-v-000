d2d58e530dc19787aef0681bb2afb3c1c0208f2c
Justin Tooley <jstooley823@gmail.com>
2017-09-20 05:12:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,8 @@ class Scraper
 
 
       links = doc.css(".social-icon-container a").collect{|line| line.attr('href')}
-      profile_index[:profile_quote] = doc.css(".profile-quote").text,
-      :bio =>doc.css(".description-holder p").text}
+      profile_index[:profile_quote] = doc.css(".profile-quote").text
+      profile_index[:bio] = doc.css(".description-holder p").text}
       links.each do |link|
             case
             when link.include?("twitter")
